77070ffc122e52a03b0b71c6ff5b6b2517025300
Stas Avdeev <ispanecfeo@gmail.com>
2021-12-25 22:45:33 +0300

    init notebook progect

---
+++ app/src/main/java/avdeev/geekbrains/data/Repo.java
@@ -0,0 +1,18 @@
+package avdeev.geekbrains.data;
+
+import java.util.List;
+
+public interface Repo {
+    // CRUD
+    // ------
+    // Create
+    // Read
+    // Update
+    // Delete
+    int  create(Note note);
+    Note read(int id);
+    void update(Note note);
+    void delete(int id);
+
+    List<Note> getAll();
+}
---
+++ app/src/main/java/avdeev/geekbrains/recycler/NoteHolder.java
@@ -0,0 +1,31 @@
+package avdeev.geekbrains.recycler;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import avdeev.geekbrains.R;
+import avdeev.geekbrains.data.Note;
+
+public class NoteHolder extends RecyclerView.ViewHolder {
+
+    private TextView title;
+    private TextView description;
+    private Note note;
+
+    public NoteHolder(@NonNull View itemView, NotesAdapter.OnNoteClickListener listener) {
+        super(itemView);
+        title = itemView.findViewById(R.id.note_title);
+        description = itemView.findViewById(R.id.note_description);
+        itemView.setOnClickListener(v -> listener.onNoteClick(note));
+    }
+
+    void bind(Note note)
+    {
+        this.note = note;
+        title.setText(note.getTitle());
+        description.setText(note.getDescription());
+    }
+}
